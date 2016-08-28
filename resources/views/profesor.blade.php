778c53ec7ca70c63f78a24772668b0f3c673154f
frutos93 <cefg93@gmail.com>
2016-08-27 20:37:47 -0500

    Interfaz profesor
    Interfaz profesor agregada.

---
+++ resources/views/profesor.blade.php
@@ -0,0 +1,24 @@
+@extends('layout')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class=" col-lg-6 col-md-6 col-sm-6 col-xs-6" >
+                <h2>Nombre:</h2>
+                <h2>Ubicacion:</h2>
+                <h2>Proyectos:</h2>
+                <ul>
+                    <li>Proyecto 1</li>
+                    <li>Proyecto 2</li>
+                    <li>Proyecto 3</li>
+                </ul>
+                <h2>Contacto: </h2>
+            </div>
+            <div class=" col-lg-6 col-md-6 col-sm-6 col-xs-6" align=right >
+                <img src="http://placehold.it/300x300">
+
+            </div>
+        </div>
+    </div>
+
+@stop
