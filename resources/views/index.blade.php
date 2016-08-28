ead987e7d19fe4d1fe79cd4cc283972317746bed
frutos93 <cefg93@gmail.com>
2016-08-27 19:51:40 -0500

    Layout views
    this.

---
+++ resources/views/index.blade.php
@@ -0,0 +1,60 @@
+@extends('layout')
+
+@section('content')
+<div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-offset-1 col-lg-3 col-md-4 col-sm-4 col-xs-4" style="background-color:white;">
+                <h1>Escuelas</h1>
+                <div class="btn-group-vertical">
+                    <button type="button" class="btn btn-primary">Escuela de Ingeniería y Ciencias</button>
+                    <br>
+                    <button type="button" class="btn btn-primary">Escuela de Educación, de Humanidades y Ciencias Sociales</button>
+                    <br>
+                    <button type="button" class="btn btn-primary">EGADE Business School</button>
+                    <br>
+                    <button type="button" class="btn btn-primary">Escuela de Gobierno y transformación pública</button>
+                    <br>
+                    <button type="button" class="btn btn-primary">Escuela de Medicina</button>
+                    <br>
+
+                </div>
+            </div>
+            <div class="col-lg-offset-1 col-lg-3 col-md-4 col-sm-4 col-xs-4" style="background-color:white;">
+                <h1>Area</h1>
+                <div class="btn-group-vertical">
+                    <button type="button" class="btn btn-primary">Tecnologías de Información</button>
+                    <br>
+                    <button type="button" class="btn btn-primary">Samsung</button>
+                    <br>
+                    <button type="button" class="btn btn-primary">Sony</button>
+                    <br>
+                    <button type="button" class="btn btn-primary">Sony</button>
+                    <br>
+                    <button type="button" class="btn btn-primary">Sony</button>
+                    <br>
+
+                </div>
+            </div>
+            <div class=" col-lg-3 col-md-4 col-sm-4 col-xs-4" style="background-color:white;">
+                <h1>Profesores</h1>
+                <table id="table_id" class="display">
+                <thead>
+                    <tr>
+                        <th>Nombre</th>
+                    </tr>
+                </thead>
+                <tbody>
+                     @foreach($teachers as $teacher)
+                    <tr>
+                        <td>{{$teacher.name}}</td>
+                    </tr>
+                    @endforeach                    
+                    
+                </tbody>
+                </table>
+            </div>
+
+
+        </div>
+    </div>
+@stop
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title></title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+{{ HTML::style('css/style.css') }}
+    <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.12/css/jquery.dataTables.css">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+    <!--NAVBAR BEGIN-->
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">PIP</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <form class="navbar-form navbar-right">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Buscar">
+                    </div>
+                    <button type="submit" class="btn btn-default">Buscar</button>
+                </form>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+    <!--NAVBAR ENDS-->
+    <!--Content-->
+    @yield('content')
+    <!--End Content-->
+
+    <!--FOOTER BEGIN--->
+    <div class="container-fluid" style="background-color:black; color:white;">
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4" align="center">
+                <h3>PIP</h3>
+                <h4>Proyecto Hackaton</h4>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4" align="center">
+                <h4>Gerardo Tenorio</h4>
+                <h4>Roberto Ruiz</h4>
+                <h4>Melissa González</h4>
+                <h4>Carlos Frutos</h4>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4" align="left">
+                <h4>Algo más de información</h4>
+            </div>
+        </div>
+    </div>
+    <!--FOOTER END--->
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+     <script type="text/javascript" charset="utf8" src="//cdn.datatables.net/1.10.12/js/jquery.dataTables.js"></script>
+    <script>
+    $(document).ready( function () {
+    $('#table_id').DataTable();
+} );</script>
+</body>
+
+</html>
